8a1fb7c4dfeb06d98645e348841a9cf35453d1b2
SumimotoTakara <96.sumimoto@gmail.com>
2020-12-25 11:54:41 +0900

    firstcommit

---
+++ dist/js/bundle91820b4a0bf6630f698f.js
@@ -0,0 +1,12 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/*!************************!*\
+  !*** ./src/ts/main.ts ***!
+  \************************/
+
+console.log("hello");
+console.log("hello ts");
+
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly90cy10b2RvLWFwcC8uL3NyYy90cy9tYWluLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7Ozs7O0FBQWE7QUFDYjtBQUNBIiwiZmlsZSI6ImpzL2J1bmRsZTkxODIwYjRhMGJmNjYzMGY2OThmLmpzIiwic291cmNlc0NvbnRlbnQiOlsiXCJ1c2Ugc3RyaWN0XCI7XG5jb25zb2xlLmxvZyhcImhlbGxvXCIpO1xuY29uc29sZS5sb2coXCJoZWxsbyB0c1wiKTtcbiJdLCJzb3VyY2VSb290IjoiIn0=
---
+++ webpack.dev.js
@@ -0,0 +1,17 @@
+const commonConfig = require("./webpack.common");
+const { merge } = require("webpack-merge");
+
+// ローカルサーバーで使うため必要
+const path = require("path");
+
+module.exports = merge(commonConfig, {
+  mode: "development",
+  // ソースマップの出力
+  devtool: "inline-source-map",
+  watch: true,
+  // HMR付きローカルサーバーの起動
+  devServer: {
+    open: true,
+    contentBase: path.resolve(__dirname, "dist"),
+  },
+});
